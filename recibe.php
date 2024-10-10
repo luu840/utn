8253069acf3371eb6703f89270ef9fb9692273ff
luu840 <lusol160506@gmail.com>
2024-10-10 17:23:29 -0300

    Add files via upload

---
+++ recibe.php
@@ -0,0 +1,44 @@
+<?php
+echo "<h1>DATOS RECIBIDOS</h1>";
+echo "</br>";
+$nombre =  $_GET['nombre'];
+
+
+echo "NOMBRE Y APELLIDO: $nombre" ;
+
+
+echo "</br>";
+
+$domicilio = $_GET ['domicilio'];
+
+echo "Domicilio : $domicilio";
+
+
+echo "</br>";
+
+$opinion = $_GET ['opinion'];
+
+echo "opinion : $opinion";
+
+echo "</br>";
+
+echo "</br>";
+
+$opi = $_GET ['opi'];
+
+echo "tu opinion es : $opi";
+
+echo "</br>";
+
+$sexo = $_GET ['sexo'];
+
+echo "sexo : $sexo";
+
+
+
+
+
+
+
+
+?>
